d1ca692e6b2ef6685b94e75214effcc2b60c9a80
tckthecreator <dsoouuzavitu@gmail.com>
2021-10-30 12:18:16 -0300

    feature(user): add user endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
